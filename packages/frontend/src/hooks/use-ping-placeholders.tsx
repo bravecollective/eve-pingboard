29e031be3e593a2f7ff21acb0ae22435f7e3614a
1adog1 <40224386+1adog1@users.noreply.github.com>
2026-04-29 22:44:18 -0400

    Missed a trailing space

--- packages/frontend/src/hooks/use-ping-placeholders.tsx
+++ packages/frontend/src/hooks/use-ping-placeholders.tsx
@@ -44,7 +44,7 @@ export function usePingPlaceholders(template: ApiPingTemplate | null, options: {
             />) followed by a countdown.
             The date-time is converted to the local timezone of whoever&apos;s viewing it,
             and the countdown updates automatically.
-            Both link to a 
+            Both link to a
             <a href="https://time.nakamura-labs.com/" target="_blank" rel="noreferrer">
               Nakamura time link
             </a>
